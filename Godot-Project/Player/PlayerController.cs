62c7af62772d09afb1c31cd823899fe85f406ae0
Gxaps <williamtetzlaff123@gmail.com>
2024-08-30 19:06:26 +1200

    Rename classes, initialize jetbrains rider

--- Godot-Project/Player/PlayerController.cs
+++ Godot-Project/Player/PlayerController.cs
@@ -1,39 +1,38 @@
 using Godot;
-using System;
-using System.Diagnostics;
+
+namespace ArchitectsInVoid.Scripts;
 
 public partial class PlayerController : Node3D
 {
 
 	// Body
-	RigidBody3D body;
+	RigidBody3D _body;
 	Node3D headPosition;
 
 	// Head
-	[Export] Node3D head;
+	[Export] Node3D _head;
 	[Export] Camera3D camera;
 
 
-	public bool Dampeners = false;
-	public bool Jetpack = true;
+	private bool _dampeners = false;
+	private bool _jetpack = true;
 
 	// Config
-	public const float Acceleration = 25.0f;
-	public const float MouseSensitivity = 0.05f;
-	public const float RollSensitivity = 50.0f;
+	private const float Acceleration = 25.0f;
+	private const float MouseSensitivity = 0.05f;
+	private const float RollSensitivity = 50.0f;
 
 	// get gravity from the world
 	public Vector3 gravity = ProjectSettings.GetSetting("physics/3d/default_gravity_vector").As<Vector3>() * ProjectSettings.GetSetting("physics/3d/default_gravity").As<float>();
 	public override void _Ready()
 	{
-		
-		body = GetNode<RigidBody3D>("Body");
-		headPosition = body.GetNode<Node3D>("HeadPosition");
+		_body = GetNode<RigidBody3D>("Body");
+		headPosition = _body.GetNode<Node3D>("HeadPosition");
 
-		head = GetNode<Node3D>("Head");
-		camera = head.GetNode<Camera3D>("Camera");
+		_head = GetNode<Node3D>("Head");
+		camera = _head.GetNode<Camera3D>("Camera");
 
-		head.Transform = headPosition.Transform;
+		_head.Transform = headPosition.Transform;
 
 		Input.MouseMode = Input.MouseModeEnum.Captured;
 	}
@@ -44,9 +43,9 @@ public partial class PlayerController : Node3D
 		if (@event is InputEventMouseMotion mouseEvent)
 		{
 
-			head.RotateObjectLocal(Vector3.Up, Mathf.DegToRad(-mouseEvent.Relative.X * MouseSensitivity));
-			head.RotateObjectLocal(Vector3.Right, Mathf.DegToRad(-mouseEvent.Relative.Y * MouseSensitivity));
-			headRelativeRotation = head.Rotation - body.Rotation;
+			_head.RotateObjectLocal(Vector3.Up, Mathf.DegToRad(-mouseEvent.Relative.X * MouseSensitivity));
+			_head.RotateObjectLocal(Vector3.Right, Mathf.DegToRad(-mouseEvent.Relative.Y * MouseSensitivity));
+			headRelativeRotation = _head.Rotation - _body.Rotation;
 
 		}
 	}
@@ -55,11 +54,11 @@ public partial class PlayerController : Node3D
 	{
 
 		// Multiply vectors in head space by this to convert to "PlayerOrigin" space
-		Basis headTransform = head.Transform.Basis.Inverse();
-		Basis bodyTransform = body.Transform.Basis.Inverse();
+		Basis headTransform = _head.Transform.Basis.Inverse();
+		Basis bodyTransform = _body.Transform.Basis.Inverse();
 
 		// Multiply head position by body transform to get the head position in PlayerOrigin space
-		head.Position = body.Position + headPosition.Position * bodyTransform;
+		_head.Position = _body.Position + headPosition.Position * bodyTransform;
 
 		// Process our key input
 		Vector3 moveVector = KeyInputProcess(delta);
@@ -70,7 +69,7 @@ public partial class PlayerController : Node3D
 
 
 
-		if (Jetpack) JetpackProcess(headTransform, bodyTransform, moveVector, delta);
+		if (_jetpack) JetpackProcess(headTransform, bodyTransform, moveVector, delta);
 		else NoJetpackProcess(delta, headTransform, bodyTransform);
 
 
@@ -89,10 +88,10 @@ public partial class PlayerController : Node3D
 		var desiredAngularChangeX = forwardBodySpace.Y;
 		var desiredAngularChangeZ = -upBodySpace.X;
 
-		body.AngularDamp = 0.0f;
-		body.AngularVelocity = new Vector3(desiredAngularChangeX, desiredAngularChangeY, desiredAngularChangeZ) * 10.0f * bodyTransform;
+		_body.AngularDamp = 0.0f;
+		_body.AngularVelocity = new Vector3(desiredAngularChangeX, desiredAngularChangeY, desiredAngularChangeZ) * 10.0f * bodyTransform;
 
-		body.LinearVelocity += GetAcceleration(moveVector.Normalized(), headTransform) * delta; // Our moveVector in PlayerOrigin space
+		_body.LinearVelocity += GetAcceleration(moveVector.Normalized(), headTransform) * delta; // Our moveVector in PlayerOrigin space
 	}
 
 	Vector3 headRelativeRotation = Vector3.Zero;
@@ -101,12 +100,12 @@ public partial class PlayerController : Node3D
 
 
 
-		head.Rotation = body.Rotation + headRelativeRotation;
+		_head.Rotation = _body.Rotation + headRelativeRotation;
 	}
 
 	private Vector3 GetAcceleration(Vector3 moveVector, Basis headTransform)
 	{
-		if (Dampeners)
+		if (_dampeners)
 		{
 			Vector3 transformedMoveVector = moveVector * headTransform;
 			double gravityLength = gravity.Length();
@@ -139,18 +138,18 @@ public partial class PlayerController : Node3D
 	private Vector3 KeyInputProcess(double delta)
 	{
 		float inputRoll = Input.GetAxis("roll_left", "roll_right");
-		head.RotateObjectLocal(Vector3.Forward, Mathf.DegToRad(inputRoll * RollSensitivity * (float)delta));
+		_head.RotateObjectLocal(Vector3.Forward, Mathf.DegToRad(inputRoll * RollSensitivity * (float)delta));
 		float inputLeftRight = Input.GetAxis("move_left", "move_right");
 		float inputUpDown = Input.GetAxis("move_down", "move_up");
 		float inputForwardBackward = Input.GetAxis("move_forward", "move_backward");
 
 		if (Input.IsActionJustPressed("toggle_dampeners"))
 		{
-			Dampeners = !Dampeners;
+			_dampeners = !_dampeners;
 		}
 		if (Input.IsActionJustPressed("toggle_jetpack"))
 		{
-			Jetpack = !Jetpack;
+			_jetpack = !_jetpack;
 		}
 
 		return new Vector3(inputLeftRight, inputUpDown, inputForwardBackward);
