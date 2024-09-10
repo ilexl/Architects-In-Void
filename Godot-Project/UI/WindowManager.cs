94f8bb770d03cdee72ca038f83654315844e9d32
Alex <109491531+ilexl@users.noreply.github.com>
2024-09-10 19:50:22 +1200

    Added window manager and window code
    Window manager can identify and add children as windows
    Window manager has buttons to show each window
    Windows have button to show and hide window as needed

--- Godot-Project/UI/WindowManager.cs
+++ Godot-Project/UI/WindowManager.cs
@@ -1,15 +1,276 @@
 using Godot;
 using System;
 
+[Tool]
 public partial class WindowManager : Node
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
+    [Export] private Window[] Windows;
+    [Export] private bool startFeature = true;
+    
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
 	{
-	}
+        if (!startFeature) { return; }
+        foreach (Window window in Windows)
+        {
+            if (window.ShowOnStart)
+            {
+                window.Show();
+            }
+            else
+            {
+                window.Hide();
+            }
+        }
+    }
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+    public Godot.Collections.Array AddInspectorButtons()
+    {
+        Godot.Collections.Array buttons = new Godot.Collections.Array();
+
+        Godot.Collections.Dictionary gw = new Godot.Collections.Dictionary
+        {
+            { "name", "Get Windows (Children)" },
+            { "icon", GD.Load("res://Testing/InspectorButtons/icon.svg") },
+            { "pressed", Callable.From(GetWindowsInInspector) }
+        };
+        buttons.Add(gw);
+
+        foreach(Window w in Windows)
+        {
+            Godot.Collections.Dictionary windowButtonShowOnly = new Godot.Collections.Dictionary
+            {
+                { "name", $"Show Only {w.wGetName()}" },
+                { "icon", GD.Load("res://Testing/InspectorButtons/icon.svg") },
+                { "pressed", Callable.From(w.wShowOnly) }
+            };
+            buttons.Add(windowButtonShowOnly);
+        }
+
+        return buttons;
+    }
+
+    // Gets the windows for this manager to manage!
+    public void GetWindowsInInspector()
+    {
+        Windows = new Window[] { };
+        int windowsAmount = 0;
+
+        foreach (var child in GetChildren())
+        {
+            Window w = child as Window;
+            bool successfullyFoundChildWindow = (w != null);
+            if (successfullyFoundChildWindow)
+            {
+                GD.Print($"Window: {w.wGetName()} added to {Name} successfully!");
+                windowsAmount++;
+            }
+        }
+        
+        Windows = new Window[windowsAmount];
+        int counter = 0;
+        foreach (var child in GetChildren())
+        {
+            Window w = child as Window;
+            bool successfullyFoundChildWindow = (w != null);
+            if (successfullyFoundChildWindow)
+            {
+                w.SetWindowManager(this);
+                Windows[counter++] = w;
+            }
+        }
+        
+    }
+    #region wmFunctions
+
+    // Show window functions (hides all others)
+
+    /// <summary>
+    /// Shows the individual window - Hides all other windows the WM manages
+    /// </summary>
+    /// <param name="window">The window to show</param>
+    public void ShowWindow(Window window)
+    {
+        foreach (Window _window in Windows)
+        {
+            if (_window == window)
+            {
+                _window.Show(); // Shows the required window
+            }
+            else
+            {
+                _window.Hide(); // Hides all others
+            }
+        }
+    }
+
+    /// <summary>
+    /// Shows the individual window - Hides all other windows the WM manages
+    /// </summary>
+    /// <param name="windowIndex">The index in windows to show</param>
+    public void ShowWindow(int windowIndex)
+    {
+        if (windowIndex >= Windows.Length)
+        {
+            GD.PushError($"Index out of range - {windowIndex} for Windows in WM...");
+        }
+        else
+        {
+            ShowWindow(Windows[windowIndex]);
+        }
+    }
+
+    /// <summary>
+    /// Shows the individual window - Hides all other windows the WM manages
+    /// </summary>
+    /// <param name="windowName">The transform name of the window to show</param>
+    public void ShowWindow(string windowName)
+    {
+        bool found = false;
+        foreach (Window window in Windows)
+        {
+            if (window.wGetName() == windowName)
+            {
+                ShowWindow(window);
+                found = true;
+            }
+        }
+        if (!found)
+        {
+            GD.PushError($"Window: {windowName} NOT found...");
+        }
+    }
+
+    // Show window functions (leaves all others as current)
+
+    /// <summary>
+    /// Shows the individual window - Leaves all other windows the WM manages
+    /// </summary>
+    /// <param name="window">The window to show</param>
+    public void ShowOnly(Window window)
+    {
+        foreach (Window _window in Windows)
+        {
+            if (_window == window)
+            {
+                _window.Show(); // Shows the required window
+            }
+        }
+    }
+
+    /// <summary>
+    /// Shows the individual window - Leaves all other windows the WM manages
+    /// </summary>
+    /// <param name="windowIndex">The index in windows to show</param>
+    public void ShowOnly(int windowIndex)
+    {
+        if (windowIndex >= Windows.Length)
+        {
+            GD.PushError($"Index out of range - {windowIndex} for Windows in WM...");
+        }
+        else
+        {
+            ShowOnly(Windows[windowIndex]);
+        }
+    }
+
+    /// <summary>
+    /// Shows the individual window - Leaves all other windows the WM manages
+    /// </summary>
+    /// <param name="windowName">The transform name of the window to show</param>
+    public void ShowOnly(string windowName)
+    {
+        bool found = false;
+        foreach (Window window in Windows)
+        {
+            if (window.wGetName() == windowName)
+            {
+                ShowOnly(window);
+                found = true;
+            }
+        }
+        if (!found)
+        {
+            GD.PushError($"Window not found - {windowName}");
+        }
+    }
+
+    // Hide All Windows Function
+
+    /// <summary>
+    /// Hides all the windows managed by the WM
+    /// </summary>
+    public void HideAll()
+    {
+        foreach (Window window in Windows)
+        {
+            window.Hide();
+        }
+    }
+
+    // Hide Specific Window (Individually Hide)
+
+    /// <summary>
+    /// Hides the individual window - Leaves all other windows the WM manages
+    /// </summary>
+    /// <param name="window">The window to hide</param>
+    public void HideOnly(Window window)
+    {
+        foreach (Window _window in Windows)
+        {
+            if (_window == window)
+            {
+                _window.Hide(); // Shows the required window
+            }
+        }
+    }
+
+    /// <summary>
+    /// Hides the individual window - Leaves all other windows the WM manages
+    /// </summary>
+    /// <param name="windowIndex">The index in windows to Hide</param>
+    public void HideOnly(int windowIndex)
+    {
+        if (windowIndex >= Windows.Length)
+        {
+            GD.PushError($"Index out of range - {windowIndex} for Windows in WM...");
+        }
+        else
+        {
+            HideOnly(Windows[windowIndex]);
+        }
+    }
+
+    /// <summary>
+    /// Hides the individual window - Leaves all other windows the WM manages
+    /// </summary>
+    /// <param name="windowName">The transform name of the window to Hide</param>
+    public void HideOnly(string windowName)
+    {
+        bool found = false;
+        foreach (Window window in Windows)
+        {
+            if (window.wGetName() == windowName)
+            {
+                HideOnly(window);
+                found = true;
+            }
+        }
+        if (!found)
+        {
+            GD.PushError($"Window not found - {windowName}");
+        }
+    }
+
+    /// <summary>
+    /// Gets all the windows as an array (readonly)
+    /// </summary>
+    /// <returns>WM windows</returns>
+    public Window[] GetWindows()
+    {
+        return Windows;
+    }
+    #endregion
+
+    
 }
