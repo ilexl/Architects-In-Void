35b1dde828a1c50aca1c3956e7e9a5aa8513a73b
Gxaps <williamtetzlaff123@gmail.com>
2024-09-14 13:48:47 +1200

    PlaceableComponent base class

---
+++ Godot-Project/VesselComponent/PlaceableComponent.cs
@@ -0,0 +1,19 @@
+using Godot;
+
+namespace ArchitectsInVoid.VesselComponent;
+
+public partial class PlaceableComponent : Node3D
+{
+	private Vector3 _placementScale;
+	
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
