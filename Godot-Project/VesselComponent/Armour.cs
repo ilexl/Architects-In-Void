9364faf8711d39bcafe3647f353e201d540991f6
Gxaps <williamtetzlaff123@gmail.com>
2024-09-14 13:54:03 +1200

    Armour, inheriting from placeable component

---
+++ Godot-Project/VesselComponent/Armour.cs
@@ -0,0 +1,14 @@
+namespace ArchitectsInVoid.VesselComponent;
+
+public partial class Armour : PlaceableComponent
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
