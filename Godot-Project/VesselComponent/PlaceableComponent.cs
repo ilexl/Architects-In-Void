74270668f8addbb8f9d20766f6f54f9acb9aed1f
Gxaps <williamtetzlaff123@gmail.com>
2024-09-14 13:59:58 +1200

    Remove redundant functions, add type enum

--- Godot-Project/VesselComponent/PlaceableComponent.cs
+++ Godot-Project/VesselComponent/PlaceableComponent.cs
@@ -2,21 +2,16 @@ using Godot;
 
 namespace ArchitectsInVoid.VesselComponent;
 
+
+public enum PlaceableComponentType
+{
+	FixedScale,
+	DynamicScale
+}
 public partial class PlaceableComponent : Node3D
 {
+	protected virtual PlaceableComponentType Type => PlaceableComponentType.FixedScale;
 	
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		
-	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
-	
-	// make an overridable function
 	protected virtual void Place(Vector3 position, Vector3 placementScale)
 	{
 		Position = position;
