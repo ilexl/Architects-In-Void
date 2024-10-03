9cd6008368ef8de4b3bcb7558a567fb4998f58cb
Gxaps <williamtetzlaff123@gmail.com>
2024-10-03 18:45:32 +1300

    Rename DebugDraw

--- Godot-Project/Debug/DebugDraw.cs
+++ Godot-Project/Debug/DebugDraw.cs
@@ -4,23 +4,23 @@ using Godot;
 
 namespace ArchitectsInVoid.Debug;
 
-public sealed partial class AiVDebug : Node
+public sealed partial class DebugDraw : Node
 {
 
     public static SceneTree SceneTree = (SceneTree)Engine.GetMainLoop();
     public static Window Root = SceneTree.GetRoot();
     
     
-    AiVDebug()
+    DebugDraw()
     {
         Root.AddChild(this);
     }
 
     #region Singleton
-    private static AiVDebug _instance = null;
+    private static DebugDraw _instance = null;
     private static readonly object Padlock = new object();
 
-    public static AiVDebug Instance
+    public static DebugDraw Instance
     {
         get
         {
@@ -28,7 +28,7 @@ public sealed partial class AiVDebug : Node
             {
                 if (_instance == null)
                 {
-                    _instance = new AiVDebug();
+                    _instance = new DebugDraw();
                 }
 
                 return _instance;
@@ -37,10 +37,29 @@ public sealed partial class AiVDebug : Node
     }
     #endregion
     #region Instanced
-        private Dictionary<MeshInstance3D, double> _debugObjects = new();
+
+    
+    private bool _cameraNormalThisFrame = false;
+    private Vector3 _cameraNormal;
+    private Vector3 CameraNormal
+    {
+        get
+        {
+            if (!_cameraNormalThisFrame)
+            {
+                _cameraNormal = Root.GetCamera3D().GlobalTransform.Basis.Z;
+                _cameraNormalThisFrame = true;
+            }
+            return _cameraNormal;
+        }
+    }
+
+    private Dictionary<MeshInstance3D, double> _debugObjects = new();
         private Dictionary<MeshInstance3D, double> _backList = new();
         public override void _Process(double delta)
         {
+            _cameraNormalThisFrame = false;
+            
             _backList.Clear();
             
             foreach (var kvp in _debugObjects)
@@ -103,8 +122,8 @@ public sealed partial class AiVDebug : Node
         Wireframe,
         Tangible
     }
-    
-    private const double DefaultDuration = 1.0 / 60;
+
+    private const double DefaultDuration = 1.0 / 60; // TODO: Replace this with update rate from project settings
     private const double DefaultThickness = 0;
     
     // Cannot make consts for structs I don't think
@@ -112,10 +131,20 @@ public sealed partial class AiVDebug : Node
     
 
     private static readonly Color DefaultColor = new Color(1.0f, 1.0f, 1.0f);
-    private static readonly Vector3 DefaultB = Vector3.Zero;
-    public static void Line(Vector3 a, [Optional]Vector3? b, [Optional]Color? color, double duration = DefaultDuration, double thickness = DefaultThickness, DLineType type = DLineType.Auto )
+    private static readonly Vector3 DefaultPos = Vector3.Zero;
+    /// <summary>
+    /// Draws a line between two points with optional parameters.
+    /// </summary>
+    /// <param name="start">Start position of line. Defaults to Vector3.Zero.</param>
+    /// <param name="end">End position of line. Defaults to Vector3.Zero.</param>
+    /// <param name="color">Color of line. Defaults to White.</param>
+    /// <param name="duration">Time until the line gets destroyed. Defaults to 1 frame.</param>
+    /// <param name="thickness">Apparent "radius" of the line. Defaults to 0.</param>
+    /// <param name="type">Auto, Tangible, or Wireframe. Tangible produces a line with a radius, wireframe produces an infinitely small yet visible line. Auto is Wireframe if 0, otherwise tangible. Defaults to Auto.</param>
+    public static void Line([Optional]Vector3? start, [Optional]Vector3? end, [Optional]Color? color, double duration = DefaultDuration, double thickness = DefaultThickness, DLineType type = DLineType.Auto )
     {
-        Vector3 finalB = b ?? DefaultB;
+        Vector3 a = start ?? DefaultPos;
+        Vector3 b = end ?? DefaultPos;
         Color finalColor = color ?? DefaultColor;
 
         type = (type == DLineType.Auto) ? (thickness == 0 ? DLineType.Wireframe : DLineType.Tangible) : type;
@@ -127,13 +156,13 @@ public sealed partial class AiVDebug : Node
         switch (type)
         {
             case DLineType.Tangible:
-                // TODO: Do cross product stuff with camera
+                UpdateLineWidth(mesh, a, b, thickness, finalColor, Instance.CameraNormal);
                 break;
             case DLineType.Wireframe:
                 mesh.SurfaceBegin(Mesh.PrimitiveType.Lines);
                 mesh.SurfaceSetColor(finalColor);
                 mesh.SurfaceAddVertex(a);
-                mesh.SurfaceAddVertex(finalB);
+                mesh.SurfaceAddVertex(b);
                 mesh.SurfaceEnd();
                 break;
         }
@@ -142,6 +171,7 @@ public sealed partial class AiVDebug : Node
         
     }
 
+    
     
     #endregion
     
---
+++ Godot-Project/Debug/Meshes/DebugMesh.cs
@@ -0,0 +1,50 @@
+using Godot;
+
+namespace ArchitectsInVoid.Debug;
+
+public partial class DebugMesh : MeshInstance3D
+{
+    
+    #region Stuff
+    private static readonly StandardMaterial3D DefaultMaterial = new()
+    {
+        ShadingMode = BaseMaterial3D.ShadingModeEnum.Unshaded,
+        VertexColorUseAsAlbedo = true,
+        Transparency = BaseMaterial3D.TransparencyEnum.Alpha,
+        CullMode = BaseMaterial3D.CullModeEnum.Disabled,
+    };
+    
+    protected Color Color;
+    public double TimeToLive = 0;
+    protected DebugDraw Instance;
+    protected ImmediateMesh IM;
+    #endregion
+    // Prevents IDE from getting angry, should never use parameterless constructor for this and derived classes
+    public DebugMesh()
+    {
+        
+    }
+    public DebugMesh(Color color, double duration, DebugDraw instance)
+    {
+        Color = color;
+        TimeToLive = duration;
+        Instance = instance;
+        IM = new ImmediateMesh();
+        MaterialOverride = DefaultMaterial;
+        Mesh = IM;
+    }
+    
+    /// <summary>
+    /// <para>Ticks the time to live down for this object.</para>
+    /// <para>Derrived objects may override this to do additional processing.</para>
+    /// <para> I.E, A debug object may be infinitely thin and needs to be remade to face the camera.</para>
+    /// </summary>
+    /// <param name="delta">Time elapsed since last check.</param>
+    /// <returns>True if it should be destroyed.</returns>
+    public virtual bool Update(double delta)
+    {
+        TimeToLive -= delta;
+        return TimeToLive <= 0;
+    }
+    
+}
