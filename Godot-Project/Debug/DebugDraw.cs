042b747af32de915d3c7eee6571b79e1fd3c40d5
Gxaps <williamtetzlaff123@gmail.com>
2024-10-03 22:58:32 +1300

    Raycast debug draw

--- Godot-Project/Debug/DebugDraw.cs
+++ Godot-Project/Debug/DebugDraw.cs
@@ -185,5 +185,22 @@ public sealed partial class DebugDraw : Node
         
     }
     #endregion
-    
+    #region DebugRaycast
+    public static void Ray(PhysicsRayQueryParameters3D query, Godot.Collections.Dictionary result, double duration = DefaultDuration)
+    {
+        Vector3 start = query.From;
+        Vector3 end = query.To;
+        
+
+        if (result.Count > 0)
+        {
+            Vector3 hit = (Vector3)result["position"];
+            Line(start, hit, Colors.Red, duration, drawOnTop:true);
+            Line(hit, end, Colors.Green, duration, drawOnTop:true);
+            return;
+        }
+        Line(start, end, Colors.Red, duration, drawOnTop:true);
+    }    
+        
+    #endregion
 }
