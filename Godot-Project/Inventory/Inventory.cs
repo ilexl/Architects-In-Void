3967e38fb891d8b123ca5daf6e24dfd5134464d8
Alex <109491531+ilexl@users.noreply.github.com>
2024-10-23 20:55:03 +1300

    Added missing scripts

---
+++ Godot-Project/Inventory/Inventory.cs
@@ -0,0 +1,10 @@
+using Godot;
+using System;
+
+/// <summary>
+/// Inventory for use in player, vessels blocks, etc...
+/// </summary>
+public partial class Inventory : Node
+{
+    // TODO: implementation
+}
