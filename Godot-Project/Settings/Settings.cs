add8905b356bce8928637243007878d2a503931b
Alex <109491531+ilexl@users.noreply.github.com>
2024-10-26 16:06:53 +1300

    FINAL fix for centering a screen

--- Godot-Project/Settings/Settings.cs
+++ Godot-Project/Settings/Settings.cs
@@ -595,6 +595,7 @@ public partial class Settings : Node
     void ApplyScreenSettings()
     {
         if (Engine.IsEditorHint()) { return; } // do NOT apply settings if run by editor (changes the actual editor window??)
+        DisplayServer.ScreenSetKeepOn(true);
         SetDisplayMode(DisplaySMode);
         SetVSync(VSync);
         DisplayServer.WindowSetSize(Resolution);
@@ -603,10 +604,8 @@ public partial class Settings : Node
         if (DisplaySMode == DisplayMode.Windowed)
         {
             // gets the center of the MAIN screen and puts the window in the center
-            var padding = new Vector2I((DisplayServer.ScreenGetSize((int)DisplayServer.ScreenPrimary).X - Resolution.X) / 2, (DisplayServer.ScreenGetSize((int)DisplayServer.ScreenPrimary).Y - Resolution.Y) / 2);
-            var centered = new Vector2I(DisplayServer.ScreenGetSize((int)DisplayServer.ScreenPrimary).X - (padding.X + Resolution.X), padding.Y + DisplayServer.WindowGetTitleSize("").Y);
-            DisplayServer.WindowSetPosition(centered);
-            DisplayServer.WindowSetCurrentScreen(DisplayServer.GetPrimaryScreen());
+            Window mainWindow = GetWindow();
+            mainWindow.MoveToCenter();
             GD.Print($"Settings: Centered window to PRIMARY DISPLAY");
         }
     }
