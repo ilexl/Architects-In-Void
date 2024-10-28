141d4dd2f5d1b4cfb4fa90524507db9d5b8d3185
Alex <109491531+ilexl@users.noreply.github.com>
2024-10-28 13:03:45 +1300

    Reverted FmodManager back to original
    Tried to initially fix the FMod Extension crashing but it crashes anyway so i reverted it

--- Godot-Project/addons/fmod/FmodManager.gd
+++ Godot-Project/addons/fmod/FmodManager.gd
@@ -1,26 +1,11 @@
 @tool
 extends Node
 
-var loop_check = 0
+
 
 func _ready():
 	process_mode = PROCESS_MODE_ALWAYS
-	if(PerformancesDisplay == null):
-		_dont_loop_forever()
-		_ready()
-		return
-	var pd = PerformancesDisplay.new()
-	if(pd == null):
-		_dont_loop_forever()
-		_ready()
-		return
-	add_child(pd)
-
-func _dont_loop_forever() -> void:
-	loop_check += 1
-	print("FmodManager: looped " + loop_check + " times")
-	if(loop_check > 1000):
-		push_error("infinite loop aborted...")
+	add_child(PerformancesDisplay.new())
 
 func _process(delta):
 	FmodServer.update()
