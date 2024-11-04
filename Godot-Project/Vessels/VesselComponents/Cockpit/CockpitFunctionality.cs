1a32e06954c7e60d716a49599a6a5646cabcbcca
Gxaps <williamtetzlaff123@gmail.com>
2024-11-04 18:07:58 +1300

    Recieve use input from player

--- Godot-Project/Vessels/VesselComponents/Cockpit/CockpitFunctionality.cs
+++ Godot-Project/Vessels/VesselComponents/Cockpit/CockpitFunctionality.cs
@@ -1,8 +1,21 @@
+using ArchitectsInVoid.Interactables;
+using ArchitectsInVoid.Player;
 using Godot;
 
 namespace ArchitectsInVoid.Vessels.VesselComponents.Cockpit;
 
 public partial class CockpitFunctionality : Node
 {
-    [Export] public StaticBody3D _interactor;
+    [Export] public InteractableObject _interactor;
+
+
+    public override void _Ready()
+    {
+        _interactor.Interacted += CockpitInteracted;
+    }
+
+    private void CockpitInteracted(PlayerController player)
+    {
+        GD.Print("Interacted by player");
+    }
 }
