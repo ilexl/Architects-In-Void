9237ab4b803e4656e67114a3185b256fc38ef72a
Gxaps <williamtetzlaff123@gmail.com>
2024-11-04 15:55:12 +1300

    Subscribeable interaction delegate, triggered by player interact

--- Godot-Project/Interactables/InteractableObject.cs
+++ Godot-Project/Interactables/InteractableObject.cs
@@ -1,3 +1,4 @@
+using ArchitectsInVoid.Player;
 using Godot;
 
 namespace ArchitectsInVoid.Interactables;
@@ -5,7 +6,8 @@ namespace ArchitectsInVoid.Interactables;
 /// <summary>
 /// Script for making physical objects glow and pop up a prompt showing it object can be interacted with
 /// </summary>
-public partial class InteractableObject : Node
+public partial class InteractableObject : StaticBody3D
 {
-    // TODO: implementation
+    public delegate void DInteracted(PlayerController player);
+    public DInteracted Interacted;
 }
