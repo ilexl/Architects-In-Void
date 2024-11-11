d662d072cdf6dc1c55cd107ea18642f04d98ebe3
Gxaps <williamtetzlaff123@gmail.com>
2024-11-11 21:02:27 +1300

    Remove logging, add property getter/setter

--- Godot-Project/Audio/FmodServer/fmod_event_wrapper.gd
+++ Godot-Project/Audio/FmodServer/fmod_event_wrapper.gd
@@ -11,21 +11,20 @@ static func re(event):
 	currentEvent = event
 #receive function
 static func rf(function):
-	print("FMODEventWrapper: Received new function name")
 	currentFunction = function
 #receive argument
 static func rx(arg):	
-	print("FMODEventWrapper: Received new argument")
 	currentArgs.append(arg)
 #finalize call
 static func fc():
-	var printString = "FMODEventWrapper: Calling function " + currentFunction + " With args: "
-	for arg in currentArgs:
-		printString += arg + ", "
-	print(printString)
-	
 	var result = currentEvent.callv(currentFunction, currentArgs)
-	
-	
 	currentArgs = []
 	return result
+
+#get property
+static func gp(property_name : String):
+	return currentEvent[property_name]
+	
+#set property
+static func sp(property_name : String, value):
+	currentEvent[property_name] = value
