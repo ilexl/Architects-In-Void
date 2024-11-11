5ca561ad6740fd570aaff58b7fb432b1fbbae0d6
Gxaps <williamtetzlaff123@gmail.com>
2024-11-12 00:08:51 +1300

    Sanity checks

--- Godot-Project/Audio/FmodServer/generic_fmod_wrapper.gd
+++ Godot-Project/Audio/FmodServer/generic_fmod_wrapper.gd
@@ -1,14 +1,14 @@
 @tool
 extends Node
 
-static var currentEvent
+static var currentObject
 static var currentFunction
 static var currentArgs = []
 
 
 #receive event
 static func re(event):
-	currentEvent = event
+	currentObject = event
 #receive function
 static func rf(function):
 	currentFunction = function
@@ -17,14 +17,17 @@ static func rx(arg):
 	currentArgs.append(arg)
 #finalize call
 static func fc():
-	var result = currentEvent.callv(currentFunction, currentArgs)
+	if not currentObject.has_method(currentFunction):
+		push_error("FmodGenericWrapper: Function in object " + type_string(typeof(currentObject)) + " not found: " + currentFunction)
+		return null
+	var result = currentObject.callv(currentFunction, currentArgs)
 	currentArgs = []
 	return result
 
 #get property
 static func gp(property_name : String):
-	return currentEvent[property_name]
+	return currentObject[property_name]
 	
 #set property
 static func sp(property_name : String, value):
-	currentEvent[property_name] = value
+	currentObject[property_name] = value
