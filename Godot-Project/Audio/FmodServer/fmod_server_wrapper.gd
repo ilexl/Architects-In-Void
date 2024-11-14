b8605b36bd07e82a466f72585edb8e55db114187
Gxaps <williamtetzlaff123@gmail.com>
2024-11-14 20:38:22 +1300

    Remove unnecessary logging

--- Godot-Project/Audio/FmodServer/fmod_server_wrapper.gd
+++ Godot-Project/Audio/FmodServer/fmod_server_wrapper.gd
@@ -7,18 +7,12 @@ static var currentArgs = []
 
 #receive function
 static func rf(function):
-	print("FMODWrapper: Received new function name")
 	currentFunction = function
 #receive argument
 static func rx(arg):	
-	print("FMODWrapper: Received new argument")
 	currentArgs.append(arg)
 #finalize call
 static func fc():
-	var printString = "FMODWrapper: Calling function " + currentFunction + " With args: "
-	for arg in currentArgs:
-		printString += arg + ", "
-	print(printString)
 	if not FmodServer.has_method(currentFunction):
 		push_error("FmodServerWrapper: Function not found: " + currentFunction)
 	var result = FmodServer.callv(currentFunction, currentArgs)
